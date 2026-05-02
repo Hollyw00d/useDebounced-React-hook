96249bdca8bf3f9a2463d67b4a04f2c4430a66fb
Matt Jennings <matt@mattjennings.net>
2026-05-02 14:10:00 -0700

    added useDebounce React code

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import StarRatings from './StarRatings.jsx';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StarRatings />
+    <App />
   </StrictMode>,
 );
